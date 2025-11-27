93e8ef559803642ea67176d3dc153848f6a57083
Данька <ekimochkindanil@gmail.com>
2025-11-27 22:40:28 +0300

    Update app.py
    новые страницы "posts and create=article"

--- Flask.py/app.py
+++ Flask.py/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, url_for 
+from flask import Flask, render_template, url_for ,request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///prog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Article(db.Model):
@@ -10,7 +11,7 @@ class Article(db.Model):
     intro = db.Column(db.String(300), nullable=False)
     title = db.Column(db.String(100), nullable=False)
     text = db.Column(db.Text (300), nullable=False)
-    intro = db.Column(db.DateTime, default=datetime.utcnow )
+    date = db.Column(db.DateTime, default=datetime.utcnow )
 
     def __repr__(self):
         return '<Article %r>' % self.id
@@ -22,13 +23,30 @@ def index():
 
 @app.route('/about')
 def about():
-        return render_template("about.html")
+    return render_template("about.html")
+@app.route('/posts')
+def post():
+    articles = Article.query.order_by(Article.date).all()
+    return render_template("posts.html",articles=articles)
 
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name,id):
-    return"User page: " + name + " - " + str(id)
+@app.route('/create-article',methods=['POST','GET'])
+def create_article():
+    if request.method == "POST":
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
 
+        article = Article(title=title,intro=intro,text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "произошла ошибка"
+    else:
+        return render_template("create-article.html")
 
 
 
